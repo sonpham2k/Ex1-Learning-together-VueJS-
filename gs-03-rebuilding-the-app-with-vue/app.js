cc96b340cd175296c6c0ec158cb35aac8772569f
ngocson <phamngocson2k2@gmail.com>
2022-07-01 11:03:57 +0700

    Ex1 Learning VueJS

---
+++ gs-03-rebuilding-the-app-with-vue/app.js
@@ -0,0 +1,34 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    },
+    deleteGoal(index) {
+      this.goals.splice(index, 1);
+    },
+    undoGoal(index) {
+      this.goals.pop(this.enteredValue);
+    }
+  }
+}).mount('#app');
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
